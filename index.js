fc293b7a71d7654ed398ac484db56e9a1ce2350a
Wendy Harris <wendyv510@hotmail.com>
2020-06-07 11:08:52 -0400

    DOMContentLoaded & initial fetch

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+document.addEventListener('DOMContentLoaded', () => { 
+    fetch("https://localhost:3000/api/v1/categories") 
+    .then(response => response.json()) 
+    .then(categories => { 
+        console.log(categories)
+    }) 
+})
