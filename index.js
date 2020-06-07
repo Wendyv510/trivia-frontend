64358113ef781b7047715dd164462618b2948807
Wendy Harris <wendyv510@hotmail.com>
2020-06-07 13:43:18 -0400

    category const

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const categoryBackend = "https://localhost:3000/api/v1/categories"
+
 document.addEventListener('DOMContentLoaded', () => { 
-    fetch("https://localhost:3000/api/v1/categories") 
+    getCategories() 
+})
+
+function getCategories() { 
+    fetch(categoryBackend) 
     .then(response => response.json()) 
     .then(categories => { 
         console.log(categories)
     }) 
-})
+}
