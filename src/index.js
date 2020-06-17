017213f9b0ad2c847aaaae34f4a123e4dd1e9e04
Wendy Harris <wendyv510@hotmail.com>
2020-06-17 14:00:33 -0400

    debugging

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const categories = "https://localhost:3000/api/v1/categories"
 const questions = "https://localhost:3000/api/v1/questions"
 
-document.addEventListener('DOMContentLoaded', () { 
+document.addEventListener('DOMContentLoaded', () => { 
     getCategories()
 }) 
 
@@ -15,8 +15,8 @@ function getCategories(){
 function renderCategories(categories) { 
         method: "POST"; 
         headers: {
-            "Content-Type": "application/json";
-            "Accept": "application/json" 
+            "Content-Type"; "application/json";
+            "Accept"; "application/json" 
         };  
         body: JSON.stringify(categories) 
     
@@ -37,40 +37,40 @@ function renderCategories(categories) {
 
     
 
-function getQuestions = () => {
+function getQuestions() {
 
     fetch(questions) 
     .then(response => response.json()) 
     .then(json => json.forEach(question =>  renderQuestion(question.description)          ))
 }
 
-function renderQuestion = () => { 
+function renderQuestion() { 
         method: "POST", 
-        headers: {"Content-Type": "application/json"} 
+        headers; {"Content-Type"; "application/json"} 
         body: JSON.stringify(questions) 
     
     fetch(questions) 
     .then(response => response.json()) 
-    .then(json => json.forEach(question => renderQuestion(this.description, this.option, this.answer)
+    .then(json => json.forEach(question => renderQuestion(this.description, this.option, this.answer)));
     
-    let score = 0 
-    document.querySelector("span.score-card")
+    let, score = 0, 
+    document.querySelector("span.score-card");
     for (let i = 0; i < questions.length; i++) {
     let question = document.querySelector("div.question-box")
     question.innerHTML = "this.description"   
     let opt1 = document.querySelector("li.opt1")
     opt1.innerHTML = "this.option"
-    opt1.addEventListener("onclick", function(e)) {
-        e.preventDefault() 
-        alert "Incorrect" 
-    } 
+    opt1.addEventListener("onclick", function(e) {
+        e.preventDefault(); 
+        alert; "Incorrect" 
+    }) 
     opt2.querySelector("li.opt2")
     opt2.innerHTML = "this.answer"
-    opt2.addEventListener("onclick", function(e)) { 
+    opt2.addEventListener("onclick", function(e) { 
         e.preventDefault(); 
         score ++; 
-        alert "Correct" 
-    }  
+        alert; "Correct" 
+    })  
         return score   
   }
 
