b708c552f0e6fbe52e10aa33a26c89c41c7e526e
Wendy Harris <wendyv510@hotmail.com>
2020-07-01 19:42:54 -0400

    refactoring

--- src/category.js
+++ src/category.js
@@ -9,27 +9,25 @@ class Category {
 
              let choose = document.getElementById("questionBox")
                  choose.innerHTML = "Choose a Category" 
-                 Category.all.forEach(category => {
-                    let category1 = document.getElementById("opt1") 
-                    category1.innerHTML = `${category.name}` 
-                    let category2 = document.getElementById("opt2")
-                    category2.innerHTML = `${category.name}` 
-        //         let ul = document.getElementById("ul") 
-        //         Category.all.forEach(category => { 
-        //             let li = document.createElement("li") 
-        //             li.innerHTML = `${category.name}`
-        //             ul.appendChild(li) 
-        //         })
-                category1.addEventListener('click', function() { 
+        //         Category.all.forEach(category => {
+        //            let category1 = document.getElementById("opt1") 
+        //            category1.innerHTML = `${category.name}` 
+        //            let category2 = document.getElementById("opt2")
+        //            category2.innerHTML = `${category.name}` 
+                 let ul = document.getElementById("ul") 
+                 Category.all.forEach(category => { 
+                     let li = document.createElement("li") 
+                     li.setAttribute("id", `${category.id}`);
+                     li.innerHTML = `${category.name}`
+                     ul.appendChild(li) 
+                     li.addEventListener('click', function() { 
                     
-                    getQuestions()
-                })
-                category2.addEventListener('click', function() { 
+                        getQuestions()
+                    })
                     
-                    getQuestions()
-                })
-
-            })         
+                 })
+                
+            //})         
     }
     
 }
--- src/question.js
+++ src/question.js
@@ -9,48 +9,44 @@ class Question {
     }
 
     static renderQuestions() { 
-        
+        let ul = document.getElementById("ul")
+        ul.innerHTML = ""
         let score = document.getElementById("score-card"); 
         let  resultContainer = document.getElementById("correct-incorrect") 
-        
-        for (let i = 0; i < questions.length; i++) {
-             //     Question.all.forEach(question => {
+        let i = 0 
+        // Focus on displaying all the questions and answers that correspond to that question
+        // Focus on only displaying ONE at a time
+        while ( i < questions.length) {
+             //   Question.all.forEach(question => {
              //   let questions = document.getElementById("questionBox")
              //    questions.innerHTML = `${question.description}` 
              //    let ul2 = document.getElementById("ul2") 
                   
-             //       let li = document.createElement("li") 
-             //       li.innerHTML = `${question.option}`
-             //        li.addEventListener('click', function() {
-             //          resultContainer.innerHTML =  "Incorrect"   
-              //       })
-             //        ul.appendChild(li)
-             //        let li1 = document.createElement("li1") 
-             //        li1.innerHTML = `${question.answer}`
-             //        li1.addEventListener('click', function() { 
-             //        resultContainer.innerHTML =  "Correct"
-             //        score ++;
-             //        ul.appendChild(li1)
-             //       }) 
-             //   })
+                   
        Question.all.forEach(question => {  
+           // question.innerHTML += 
+           // you might even want to create an element here 
         let questions = document.getElementById("questionBox")
         questions.innerHTML = `${question.description}`   
-        let opt1 = document.getElementById("opt1")
-        opt1.innerHTML = `${question.option}`
-        opt1.addEventListener('click', function()  {
-            resultContainer.innerHTML =  "Incorrect"
-            ++questions  
-        })
-        let opt2 = document.getElementById("opt2")
-        opt2.innerHTML = `${question.answer}`
-        opt2.addEventListener('click', function()  { 
-            resultContainer.innerHTML =  "Correct"
-            ++questions
-            score ++;
-            
+        
+        let li = document.createElement("li") 
+        li.innerHTML = `${question.option}`
+        ul.appendChild(li)
+         li.addEventListener('click', function() {
+           resultContainer.innerHTML =  "Incorrect"
+            i++ 
+         })
+         
+         let li1 = document.createElement("li1") 
+         li1.innerHTML = `${question.answer}`
+         ul.appendChild(li1)
+         li1.addEventListener('click', function() { 
+         resultContainer.innerHTML =  "Correct"
+         score ++;
+         i++
+         
         }) 
-        })    
+       })    
       }
     }
 }
