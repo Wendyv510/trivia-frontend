84f7729bcf8f754e12c1675959c69f912d055c27
Wendy Harris <wendyv510@hotmail.com>
2020-07-02 13:16:52 -0400

    reworking render questions

--- src/question.js
+++ src/question.js
@@ -9,14 +9,14 @@ class Question {
     }
 
     static renderQuestions() { 
-        let ul = document.getElementById("ul")
-        ul.innerHTML = ""
+        let ul2 = document.getElementById("ul2")
+        ul2.innerHTML = ""
         let score = document.getElementById("score-card"); 
         let  resultContainer = document.getElementById("correct-incorrect") 
         let i = 0 
         // Focus on displaying all the questions and answers that correspond to that question
         // Focus on only displaying ONE at a time
-        while ( i < questions.length) {
+             while ( i < Question.all.length) {
              //   Question.all.forEach(question => {
              //   let questions = document.getElementById("questionBox")
              //    questions.innerHTML = `${question.description}` 
@@ -26,12 +26,13 @@ class Question {
        Question.all.forEach(question => {  
            // question.innerHTML += 
            // you might even want to create an element here 
-        let questions = document.getElementById("questionBox")
-        questions.innerHTML = `${question.description}`   
-        
+        let categoryQuestions = document.getElementById("category-questions")
+        let questions = document.createElement("questionBox")
+        questions.innerHTML += `${question.description}`   
+        categoryQuestions.appendChild(questions)
         let li = document.createElement("li") 
         li.innerHTML = `${question.option}`
-        ul.appendChild(li)
+        ul2.appendChild(li)
          li.addEventListener('click', function() {
            resultContainer.innerHTML =  "Incorrect"
             i++ 
@@ -39,7 +40,7 @@ class Question {
          
          let li1 = document.createElement("li1") 
          li1.innerHTML = `${question.answer}`
-         ul.appendChild(li1)
+         ul2.appendChild(li1)
          li1.addEventListener('click', function() { 
          resultContainer.innerHTML =  "Correct"
          score ++;
